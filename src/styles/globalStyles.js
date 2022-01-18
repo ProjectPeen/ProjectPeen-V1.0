b16a73274e7c5848c729e46e74aa79274c2621ff
ProjectPeen <97575699+ProjectPeen@users.noreply.github.com>
2022-01-18 14:25:05 -0500

    Add files via upload

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+// Used for wrapping a page component
+export const Screen = styled.div`
+  background-color: var(--primary);
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+// Used for providing space between components
+export const SpacerXSmall = styled.div`
+  height: 5%;
+  width: 5%;
+`;
+
+// Used for providing space between components
+export const SpacerSmall = styled.div`
+  height: 10%;
+  width: 10%;
+`;
+
+// Used for providing space between components
+export const SpacerMedium = styled.div`
+  height: 15%;
+  width: 15%;
+`;
+
+// Used for providing space between components
+export const SpacerLarge = styled.div`
+  height: 20%;
+  width: 20%;
+`;
+
+// Used for providing a wrapper around a component
+export const Container = styled.div`
+  display: flex;
+  flex: ${({ flex }) => (flex ? flex : 0)};
+  flex-direction: ${({ fd }) => (fd ? fd : "column")};
+  justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
+  align-items: ${({ ai }) => (ai ? ai : "flex-start")};
+  background-color: ${({ test }) => (test ? "pink" : "none")};
+  width: 100%;
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+`;
+
+export const TextTitle = styled.p`
+  color: var(--primary-text);
+  font-size: 38px;
+  font-weight: 500;
+  line-height: 1.6;
+`;
+
+export const TextSubTitle = styled.p`
+  color: var(--primary-text);
+  font-size: 28px;
+  line-height: 1.6;
+`;
+
+export const TextDescription = styled.p`
+  color: var(--primary-text);
+  font-size: 26px;
+  line-height: 1.6;
+`;
+
+export const TextTiny = styled.p`
+  color: var(--primary-text);
+  font-size: 16px;
+  line-height: 1.6;
+`;
+
+export const StyledClickable = styled.div`
+  :active {
+    opacity: 0.6;
+  }
+`;
